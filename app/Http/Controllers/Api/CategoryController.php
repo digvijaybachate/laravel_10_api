ac216a31936f7bb1eac87a78620e22ae1ef26859
vtdigvijay <digvijay@ventture.com>
2024-01-29 18:10:34 +0530

    Initial commit

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function list()
+    {
+        return CategoryResource::collection(Category::all());
+    }
+
+    public function index()
+    {
+        return  CategoryResource::collection(Category::all());
+    }
+
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+}
